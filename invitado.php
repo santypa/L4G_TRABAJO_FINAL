34da5603dd56c5db39197041691740898746e8a4
santy patiño <santyp638@gmail.com>
2020-04-14 10:55:55 -0500

    inicio trabajo final santy

---
+++ invitado.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang=es>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    
+    <link rel="stylesheet" href="css/styleS.css">  
+    <link rel="icon"href="ico/www.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+</head>
+
+<body>
+    
+    <div class="tobar" id="pos">
+
+        <img src="ico/a.png" class="avatar" alt="Avatar Image" > 
+        
+         <div id="registrar">
+         <img src="ico/reg.png" class="reg" alt="Avatar Image">
+         <input type="submit" value="Registrar" id="regt"class="animated infinite pulse delay-2s" >
+         
+         </div>
+         
+         <div id="invitado">
+         <img src="ico/v.png" class="inv" alt="Avatar Image"> 
+         <input type="submit" value="Invitado" id="invi" class="animated infinite pulse delay-2s"  >
+        
+         </div>
+
+    </div >    
+
+
+</body>    
+
+</html>
+
---
+++ registro.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang=es>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    
+    <link rel="stylesheet" href="css/style_registro.css">  
+    <link rel="icon"href="ico/www.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+</head>
+
+<body>
+    
+    <div class="tobar" id="pos">
+
+  
+        <img src="ico/v.png" class="avatar" alt="Avatar Image"> 
+        
+         <div id="Nom">
+         
+            <input type="text" name="nombre" placeholder="Nombre" id="l1" required>
+            <input type="text" name="apellido" placeholder="Apellido" id="l2" required>
+            <input type="email" name="email" placeholder="Email" id="l3"  required>
+            <div id="gma">
+            <img src="ico/gmail.png" class="animated infinite pulse delay-2s" alt="Avatar Image" id="gm"> 
+            </div>
+            
+
+            <input type="password" name="clave" placeholder="Contraseña" id="l4"  required>
+            <img src="ico/hacker.png" class="animated infinite pulse delay-2s" alt="Avatar Image" id="ps"> 
+
+            <input type="email" name="celular" placeholder="Celular" id="l5"  required>
+            <img src="ico/telefono.png" class="animated infinite pulse" alt="Avatar Image" id="te"> 
+
+            <input type="email" name="direccion" placeholder="Direccion" id="l6" required>
+            <img src="ico/map.png" class="animated infinite pulse" alt="Avatar Image" id="ma"> 
+            
+            <input type="email" name="ciudad" placeholder="Ciudad" id="l7" required>
+            <img src="ico/ciu.png" class="animated infinite pulse" alt="Avatar Image" id="ci"> 
+
+         </div>
+        
+
+         <div id="registrar">
+         <img src="ico/reg.png" class="reg" alt="Avatar Image">
+         <input type="submit" value="Registrar" id="regt"class="animated infinite pulse delay-2s" >
+         </div>
+
+    </div >    
+
+
+</body>    
+
+</html>
+
+<?php
+
+//<img src="ico/hacker.png" class="animated infinite pulse" alt="Avatar Image" id="ps"> 
+//<img src="ico/telefono.png" class="animated infinite pulse" alt="Avatar Image" id="te"> 
+//<img src="ico/map.png" class="animated infinite pulse" alt="Avatar Image" id="ma"> 
+//<img src="ico/ciu.png" class="animated infinite pulse" alt="Avatar Image" id="ci"> 
+?>
